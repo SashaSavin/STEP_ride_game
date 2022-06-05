e09036df53399651941c345dc2d4e6f770a9180b
sashasavin <praise1234@mail.ru>
2022-06-06 02:10:10 +0300

    add more func5

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ pygame.display.set_caption("Don't crush my car, dude!")  # название иг
 # цвета
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (255, 0, 0)
+red = (200, 0, 0)
+green = (0,200,0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
 
 # модуль для времени, чтобы мониторить кадры в секунду
 clock = pygame.time.Clock()
@@ -67,6 +70,48 @@ def crash():
     message_display('You Crashed')
 
 
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
+
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf', 100)
+        TextSurf, TextRect = text_objects("Don't crash my car", largeText)
+        TextRect.center = ((display_width / 2), (display_height / 2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("GO!", 150, 450, 100, 50, green, bright_green, game_loop)
+
+
+        pygame.display.update()
+        clock.tick(15)
+
+
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
@@ -78,14 +123,19 @@ def game_loop():
 
     thing_startx = random.randrange(0, display_width)
     thing_starty = -600
-    thing_speed = 7
+    thing_speed = 4
     thing_width = 100
     thing_height = 100
+
+    thingCount = 1
+
 
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameExit = True
+                pygame.quit()
+                quit()
 
             # управление
             if event.type == pygame.KEYDOWN:
@@ -139,6 +189,7 @@ def game_loop():
         clock.tick(60)
 
 
+game_intro()
 game_loop()
 pygame.quit()
 quit()
